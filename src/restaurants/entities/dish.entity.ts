db9fa29a150b0b49e46c3b4aa3be06fa18e07da2
thewoowon <thewoowon@naver.com>
2022-07-16 19:05:31 +0900

    20220716_UBEREATS

--- src/restaurants/entities/dish.entity.ts
+++ src/restaurants/entities/dish.entity.ts
@@ -4,18 +4,26 @@ import { CoreEntity } from "src/common/entities/core.entity";
 import { Column, Entity, ManyToOne, RelationId } from "typeorm";
 import { Restaurant } from "./restaurant.entity";
 
-@InputType('DishOptionInputType',{isAbstract:true})
+@InputType('DishChoiceInputType',{isAbstract:true})
 @ObjectType()
- export class DishChoice{
+export class DishChoice{
 
     @Field(type => String)
     name:string;
 
-    @Field(type => [String],{nullable:true})
-    choices?:string[];
-
     @Field(type => Int,{nullable:true})
     extra?:number;
+}
+
+@InputType('DishOptionInputType', { isAbstract: true })
+@ObjectType()
+export class DishOption {
+  @Field(type => String)
+  name: string;
+  @Field(type => [DishChoice], { nullable: true })
+  choices?: DishChoice[];
+  @Field(type => Int, { nullable: true })
+  extra?: number;
 }
 
 
@@ -53,7 +61,7 @@ export class Dish extends CoreEntity{
     @RelationId((dish:Dish)=> dish.restaurant)
     restaurantId:number;
 
-    @Field(type => [DishChoice],{nullable:true})
+    @Field(type => [DishOption],{nullable:true})
     @Column({type:"json",nullable:true})
-    options:DishChoice[]
+    options?:DishOption[]
 }
