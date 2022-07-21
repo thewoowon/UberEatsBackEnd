bb6278fa3d25949fe371a91139e32af48a1c5912
thewoowon <thewoowon@naver.com>
2022-07-21 19:49:14 +0900

    20220721_UBEREATS_PAYMENTS

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,13 +1,59 @@
 import { Injectable } from "@nestjs/common";
-import { Mutation } from "@nestjs/graphql";
+import { Args, Mutation } from "@nestjs/graphql";
 import { InjectRepository } from "@nestjs/typeorm";
+import { AuthUser } from "src/auth/auth-user.decorator";
+import { Role } from "src/auth/role.decorator";
+import { Restaurant } from "src/restaurants/entities/restaurant.entity";
+import { User } from "src/users/entities/user.entity";
 import { Repository } from "typeorm";
+import { CreatePaymentInput, CreatePaymentOutput } from "./dtos/create-payment.dto";
 import { Payment } from "./entities/payment.entity";
 
 @Injectable()
 export class PaymentService{
     constructor(
         @InjectRepository(Payment)
-        private readonly payments:Repository<Payment>
+        private readonly payments:Repository<Payment>,
+        @InjectRepository(Restaurant)
+        private readonly restaurants:Repository<Restaurant>
     ){}
+    async createPayment(
+        owner:User,
+        {transactionId,restaurantId}:CreatePaymentInput
+        ):Promise<CreatePaymentOutput>{
+            try{
+                const restaurant = await this.restaurants.findOne({
+                    where:{
+                        id:restaurantId
+                    }
+                });
+                if(!restaurant){
+                    return{
+                        ok:false,
+                        error:"Restaurant not Found",
+                    }
+                }
+                if(restaurant.ownerId !== owner.id)
+                {
+                    return{
+                        ok:false,
+                        error:"You are not allowed to do this."
+                    }
+                }
+                await this.payments.save(this.payments.create({
+                    transactionId:transactionId,
+                    user:owner,
+                    restaurant:restaurant
+                }));
+                return{
+                    ok:true,
+                }
+            }
+            catch(e){
+                return{
+                    ok:false,
+                    error:"You Could not create payment"
+                }
+            }
+    }
 }
